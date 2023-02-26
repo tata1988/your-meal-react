eca20a804f04eadb0ce8151b29b190238c6ebeb8
Tatyana <nikolina.tatyana@yandex.ru>
2023-02-27 00:29:42 +0500

    логика работы корзины

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice.js";
+import orderReducer, { localStorageMiddleware } from "./order/orderSlice.js";
 
 export default configureStore({
     reducer: {
         category: categoryReducer,
+        product: productReducer,
+        order: orderReducer
     },
+
+    middleware: getDefaultMiddleware => 
+        getDefaultMiddleware().concat(localStorageMiddleware),
 });
